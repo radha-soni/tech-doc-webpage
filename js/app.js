2f681168abbf7e3efd85eddd8da1e3d25bde47ff
radha-soni <er.radha17@gmail.com>
2019-08-12 23:18:12 +0530

    ui: aside panel responsive

--- js/app.js
+++ js/app.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $("#menu-btn").click(function() {
     $("#nav-link-wrapper").toggleClass("curtain-down");
   });
+
+  $("#sidebar-toggle-btn").click(function() {
+    $("aside").toggleClass("curtain-left");
+    $("#sidebar-toggle-chevron").toggleClass("fa-angle-left");
+    $("#sidebar-toggle-chevron").toggleClass("fa-angle-right");
+  });
 });
